c01709f09916489c5fe7a8bf4deb5d11d1846992
Robert Lam <robert.lam22@gmail.com>
2025-09-04 10:52:49 +1000

    Van Detail page

---
+++ src/components/VanDetail.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+export function VanDetail() {
+    const params = useParams();
+    const [van, setVan] = useState(null);
+
+    useEffect(() => {
+        async function apiCall() {
+            const response = await fetch(`/api/vans/${params.id}`);
+            const data = await response.json();
+            setVan(data.vans);
+        }
+        apiCall();
+    }, [params.id]);
+
+    return (
+        <div className="van-detail-container">
+            {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} />
+                    <i className={`van-type ${van.type} selected`}>
+                        {van.type}
+                    </i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price">
+                        <span>${van.price}</span>/day
+                    </p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+            ) : (
+                <h2>Loading...</h2>
+            )}
+        </div>
+    );
+}
--- src/components/Vans.jsx
+++ src/components/Vans.jsx
@@ -1,17 +1,42 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export function Vans() {
-    const [vansData, setVansData] = useState(null);
+    const [vansData, setVansData] = useState([]);
 
     useEffect(() => {
         async function apiCall() {
             const response = await fetch('/api/vans');
             const data = await response.json();
-            console.log(data);
-            setVansData(data);
+            setVansData(data.vans);
         }
         apiCall();
     }, []);
 
-    return <h1>Vans page goes here</h1>;
+    const vansArr = vansData.map((van) => (
+        <div className="van-card" key={van.name}>
+            <Link to={`/vans/${van.id}`}>
+                <img src={van.imageUrl} alt={van.name} className="van-image" />
+                <div className="van-info">
+                    <h3 className="van-name">{van.name}</h3>
+                    <p className="van-price">
+                        ${van.price} <span>/day</span>
+                    </p>
+                </div>
+                <span className="van-tag">{van.type}</span>
+            </Link>
+        </div>
+    ));
+
+    return (
+        <>
+            <section>
+                <h1>Explore our van options</h1>
+                <button>Simple</button>
+                <button>Luxury</button>
+                <button>Rugged</button>
+            </section>
+            <div className="van-grid">{vansArr}</div>
+        </>
+    );
 }
